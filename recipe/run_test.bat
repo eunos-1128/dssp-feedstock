4e7324669e020d1582b967f8fb458c896b004453
UENO, M. <eunosium-1128@live.jp>
2025-06-13 06:30:14 +0000

    Add Windows command support for mkdssp in test imports

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,5 +2,5 @@
 
 @REM copy "%PREFIX%\libcifpp\share\libcifpp\*" "dssp\test\"
 cd dssp\test
-mkdssp --output-format dssp 1cbs.cif.gz 1cbs-cif.dssp
+mkdssp.exe --output-format dssp 1cbs.cif.gz 1cbs-cif.dssp
 findstr /C:"CELLULAR RETINOIC ACID BINDING PROTEIN TYPE II" 1cbs-cif.dssp >nul
