615304af237c7eb5ab5f314aa2b7bdbc24b5bf9e
UENO, M. <eunosium-1128@live.jp>
2025-06-13 06:51:37 +0000

    Fix CMake install prefix in bld.bat for dssp build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,6 +18,7 @@ set CMAKE_GENERATOR=Ninja
 
 pushd dssp
 cmake -S . -B build %CMAKE_ARGS% ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DCMAKE_CXX_FLAGS="%CXXFLAGS%" ^
     -DINSTALL_LIBRARY=ON ^
     -DBUILD_PYTHON_MODULE=ON ^
