da3fe4e881d1c4faae32f77eb0ecf025aae5937f
UENO, M. <eunosium-1128@live.jp>
2025-07-02 03:07:58 +0000

    Update target platform check in build.sh for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-if [[ "$target_platform" == "osx-"* ]]; then
+if [[ "$target_platform" == "osx-arm64" ]]; then
     export ATOMIC_BUILTIN_FLAG="-D_CXX_ATOMIC_BUILTIN_EXITCODE=0"
 fi
 
