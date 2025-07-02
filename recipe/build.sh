637dc656474276bafb62fc21731e5e4d71ef69e0
UENO, M. <eunosium-1128@live.jp>
2025-07-02 01:56:18 +0000

    Update CMake configuration to set `Python_ROOT_DIR` and `CMAKE_PREFIX_PATH`

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,15 +6,17 @@ if [[ "$target_platform" == "osx-arm64" ]]; then
     export ATOMIC_BUILTIN_FLAG="-D_CXX_ATOMIC_BUILTIN_EXITCODE=0"
 fi
 
-cmake -S . -B build ${CMAKE_ARGS} \
+cmake -S . -B build \
+    ${CMAKE_ARGS} \
+    -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_CXX_STANDARD=20 \
     -DBUILD_TESTING=OFF \
     -DINSTALL_LIBRARY=ON \
     -DBUILD_PYTHON_MODULE=ON \
+    -DPython_ROOT_DIR="${PREFIX}" \
     -DCIFPP_DATA_DIR='' \
     -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
-    -DPYTHON_SITE_PACKAGES="${SP_DIR}" \
     ${ATOMIC_BUILTIN_FLAG}
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 cmake --install build
