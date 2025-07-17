667b395cca525374fd28a86276c861723c2f1022
UENO, M. <eunosium-1128@live.jp>
2025-07-17 04:02:18 +0000

    Add activation and deactivation scripts for LIBCIFPP_DATA_DIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ if [[ "$build_platform" != "$target_platform" ]]; then
     export ATOMIC_BUILTIN_FLAG="-D_CXX_ATOMIC_BUILTIN_EXITCODE=0"
 fi
 
+# Workaround due to `CIFPP_DATA_DIR` set to an empty string in libcifpp conda package
 sed -i.bak 's|${CIFPP_DATA_DIR}/mmcif_pdbx.dic|$ENV{PREFIX}/share/libcifpp/mmcif_pdbx.dic|g' CMakeLists.txt
 
 cmake -S . -B build \
@@ -20,3 +21,12 @@ cmake -S . -B build \
     "${ATOMIC_BUILTIN_FLAG}"
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 cmake --install build
+
+
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    # activaton and deactivation scripts
+    mkdir -p "${PREFIX}/etc/conda/activate.d"
+    mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+    cp -f "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
+    cp -f "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+fi
