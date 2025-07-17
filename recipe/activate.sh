8829da84f2fe64567cb4058cd8cc36d8e188cd4e
UENO, M. <eunosium-1128@live.jp>
2025-07-17 10:15:23 +0900

    Use dynamic linking for libcifpp and libmcfp for linux and macOS builds

--- recipe/activate.sh
+++
@@ -1,2 +0,0 @@
-export LIBCIFPP_DATA_DIR="${CONDA_PREFIX}/share/libcifpp"
-echo "LIBCIFPP_DATA_DIR has been set to: ${LIBCIFPP_DATA_DIR}"
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,21 +8,13 @@ fi
 
 cmake -S . -B build \
     ${CMAKE_ARGS} \
-    -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_CXX_STANDARD=20 \
     -DBUILD_TESTING=OFF \
     -DINSTALL_LIBRARY=ON \
     -DBUILD_PYTHON_MODULE=ON \
     -DPython_ROOT_DIR="${PREFIX}" \
-    -DCIFPP_DATA_DIR='' \
     -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
-    ${ATOMIC_BUILTIN_FLAG}
+    "${ATOMIC_BUILTIN_FLAG}"
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 cmake --install build
-
-# activaton and deactivation scripts
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cp -f "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
-cp -f "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
