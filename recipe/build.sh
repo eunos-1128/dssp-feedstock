f1c7e933872d8caf440546c5846d74cabd54a777
UENO, M. <eunosium-1128@live.jp>
2025-07-17 17:07:41 +0900

    Use dynamic linking for libcifpp and libmcfp on linux (except ppc64le) and osx builds (#26)
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,26 +3,33 @@
 set -exo pipefail
 
 if [[ "$build_platform" != "$target_platform" ]]; then
-    export ATOMIC_BUILTIN_FLAG="-D_CXX_ATOMIC_BUILTIN_EXITCODE=0"
+    EXTRA_ARGS="-D_CXX_ATOMIC_BUILTIN_EXITCODE=0"
+fi
+
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    EXTRA_ARGS="${EXTRA_ARGS} -DCIFPP_DATA_DIR=''"
+else
+    # Workaround due to `CIFPP_DATA_DIR` set to an empty string in libcifpp conda package
+    sed -i.bak 's|${CIFPP_DATA_DIR}/mmcif_pdbx.dic|$ENV{PREFIX}/share/libcifpp/mmcif_pdbx.dic|g' CMakeLists.txt
 fi
 
 cmake -S . -B build \
     ${CMAKE_ARGS} \
-    -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_CXX_STANDARD=20 \
     -DBUILD_TESTING=OFF \
     -DINSTALL_LIBRARY=ON \
     -DBUILD_PYTHON_MODULE=ON \
     -DPython_ROOT_DIR="${PREFIX}" \
-    -DCIFPP_DATA_DIR='' \
     -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
-    ${ATOMIC_BUILTIN_FLAG}
+    ${EXTRA_ARGS}
 cmake --build build --config Release --parallel "${CPU_COUNT}"
 cmake --install build
 
-# activaton and deactivation scripts
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cp -f "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
-cp -f "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    # activaton and deactivation scripts
+    mkdir -p "${PREFIX}/etc/conda/activate.d"
+    mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+    cp -f "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
+    cp -f "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+fi
