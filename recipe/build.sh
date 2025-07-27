3fad011daada1baaf01ecebfbf27554238d98f2a
UENO, M. <eunosium-1128@live.jp>
2025-07-27 00:58:49 +0000

    Update build script to include additional flags and install scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,10 +11,13 @@ cmake -S . -B build \
     -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
     -DCMAKE_CXX_STANDARD=20 \
+    -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TESTING=OFF \
     -DINSTALL_LIBRARY=ON \
     -DBUILD_PYTHON_MODULE=ON \
     -DPython_ROOT_DIR="${PREFIX}" \
+    -DCIFPP_DOWNLOAD_CCD=ON \
+    -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF \
     -DCIFPP_DATA_DIR='' \
     -DCIFPP_SHARE_DIR="${PREFIX}/share/libcifpp" \
     ${ATOMIC_BUILTIN_FLAG}
@@ -24,5 +27,7 @@ cmake --install build
 # activaton and deactivation scripts
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cp -f "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
-cp -f "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+install -m 644 "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/env_vars.sh"
+install -m 644 "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/env_vars.sh"
+install -m 644 "${RECIPE_DIR}/activate.fish" "${PREFIX}/etc/conda/activate.d/env_vars.fish"
+install -m 644 "${RECIPE_DIR}/deactivate.fish" "${PREFIX}/etc/conda/deactivate.d/env_vars.fish"
